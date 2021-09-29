12636eec078021d6527bb067906945c97f4d290c
Nconst <eberzin.maxim@yandex.ru>
2021-09-30 00:38:58 +0300

    Kata solution
    Added actual code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            Console.WriteLine(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase));
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
